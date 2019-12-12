f5730283743618179876062a180d9cac2b624b2a
Leszek <leszek.synowiec@gmail.com>
2019-12-12 14:13:20 +0100

    fixed problem with undefined return value

--- js/script.js
+++ js/script.js
@@ -3,28 +3,31 @@ const buttonPaper = document.getElementById('button-paper');
 const buttonScissors = document.getElementById('button-scissors');
 
 let computerMoves = ["kamień", "papier", "nożyce"];
-let playerInput ;
-let i = Math.floor(Math.random()*computerMoves.length)
-const computerMove = computerMoves[i];
+let i = Math.floor(Math.random()*computerMoves.length);
+let playerInput;
+
  
 console.log(i);
 console.log(playerInput)
 
 buttonPaper.addEventListener('click', () => {
     playerInput = 1;
+    i = Math.floor(Math.random()*computerMoves.length);
     clearMessages();
-    printMessage(`komputer wybrał ${computerMove}`);
+    printMessage(`komputer wybrał ${computerMoves[i]}`);
     checkResult(playerInput, i);
 })
 buttonRock.addEventListener('click', () => {
     playerInput = 0;
+    i = Math.floor(Math.random()*computerMoves.length);
     clearMessages();
-    printMessage(`komputer wybrał ${computerMove}`);
+    printMessage(`komputer wybrał ${computerMoves[i]}`);
     checkResult(playerInput, i);
 })
 buttonScissors.addEventListener('click', () => {
     playerInput = 2;
+    i = Math.floor(Math.random()*computerMoves.length);
     clearMessages();
-    printMessage(`komputer wybrał ${computerMove}`);
+    printMessage(`komputer wybrał ${computerMoves[i]}`);
     checkResult(playerInput, i);
 })
